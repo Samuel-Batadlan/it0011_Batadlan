c7673792327e13a9b7cb31a29e82d95e0c52124d
Samuel-Batadlan <jsamuelb.0301@gmail.com>
2025-02-01 19:03:50 +0800

    Revert "This is the Technical Activity 1 02/01/2005"
    This reverts commit 4c24cbf1740599522c6c43372096257734947968.

--- TA1/TA1_2.py
+++
@@ -1,9 +0,0 @@
-user_input = input ("Please Enter some Strings: ")
-
-digit_sum = 0
-
-for char in user_input:
-    if char.isdigit():
-        digit_sum += int(char)
-    
-print("Sum of Digits: ", digit_sum)
